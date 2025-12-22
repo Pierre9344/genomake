ee43a703a375a43e4fb4200ca987e84fae1168f2
PS_Alienware\pierre <pierre.solomon3@gmail.com>
2025-12-22 09:45:21 +0100

    genomake initialization. chromake pipeline initialization with functions to create and modify config files and associated samplesheet.

---
+++ src/genomake/__init__.py
@@ -0,0 +1,16 @@
+"""
+This is the genomake package.
+
+Genomake (Genomic make is a collection of functions and snakemake pipelines to analyze genomic data.
+
+Currently only implement the chromake pipeline.
+"""
+
+
+
+# load subpackages next when importing the main package
+from .pipelines import chromake
+
+# make subpackage listed public so that they can be loaded with
+# from <this_package> import *
+__all__ = ["chromake"]
