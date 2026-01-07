30cdb3fa0f0accc0163a2739b28edb1bf7f58142
Pierre9344 <pierre.solomon3@gmail.com>
2026-01-07 10:15:57 +0100

    no message

--- src/genomake/pipelines/chromake/scripts/paths.py
+++ src/genomake/pipelines/chromake/scripts/paths.py
@@ -20,11 +20,11 @@ def get_all_fastq_related_paths(cfg: dict, mode: str):
                 res.append(base / inp["R2"])
         elif mode == "fastqc_raw":
             for sample in sequencing_data.get("SAMPLES", {}).values():
-                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R1"]).name).replace(".gz", ".html"))
-                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R2"]).name).replace(".gz", ".html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R1"]).name).replace("fastq.gz", "fastqc.html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R2"]).name).replace("fastq.gz", "fastqc.html"))
             for inp in sequencing_data.get("INPUT", {}).values():
-                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R1"]).name).replace(".gz", ".html"))
-                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R2"]).name).replace(".gz", ".html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R1"]).name).replace("fastq.gz", "fastqc.html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R2"]).name).replace("fastq.gz", "fastqc.html"))
         elif mode == "multiqc_raw":
             res.append(base / "QC/MULTIQC/RAW/multiqc_report.html")
         elif mode == "cutadapt":
@@ -36,11 +36,11 @@ def get_all_fastq_related_paths(cfg: dict, mode: str):
                 res.append(base / "TRIMMED" / Path(inp["R2"]).name)
         elif mode == "fastqc_trimmed":
             for sample in sequencing_data.get("SAMPLES", {}).values():
-                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(sample["R1"]).name).replace(".gz", ".html"))
-                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(sample["R2"]).name).replace(".gz", ".html"))
+                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(sample["R1"]).name).replace("fastq.gz", "fastqc.html"))
+                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(sample["R2"]).name).replace("fastq.gz", "fastqc.html"))
             for inp in sequencing_data.get("INPUT", {}).values():
-                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(inp["R1"]).name).replace(".gz", ".html"))
-                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(inp["R2"]).name).replace(".gz", ".html"))
+                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(inp["R1"]).name).replace("fastq.gz", "fastqc.html"))
+                res.append(base / "QC/FASTQC/TRIMMED" / str(Path(inp["R2"]).name).replace("fastq.gz", "fastqc.html"))
         elif mode == "multiqc_trimmed":
             res.append(base / "QC/MULTIQC/TRIMMED/multiqc_report.html")
         else:
@@ -64,11 +64,11 @@ def get_sequencing_fastq_related_paths(cfg: dict,
                 res.append(base / inp["R2"])
         elif mode == "fastqc_raw":
             for sample in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
-                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R1"]).name).replace(".gz", ".html"))
-                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R2"]).name).replace(".gz", ".html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R1"]).name).replace("fastq.gz", "fastqc.html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(sample["R2"]).name).replace("fastq.gz", "fastqc.html"))
             for inp in cfg["SEQUENCING"][project_name].get("INPUT", {}).values():
-                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R1"]).name).replace(".gz", ".html"))
-                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R2"]).name).replace(".gz", ".html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R1"]).name).replace("fastq.gz", "fastqc.html"))
+                res.append(base / "QC/FASTQC/RAW" / str(Path(inp["R2"]).name).replace("fastq.gz", "fastqc.html"))
         elif mode == "multiqc_raw":
             res.append(base / "QC/MULTIQC/RAW/multiqc_report.html")
         
