9e217d95ccc7003cf2d0ddce246170e961dec696
Pierre9344 <pierre.solomon3@gmail.com>
2026-01-08 11:12:12 +0100

    no message

--- src/genomake/pipelines/chromake/scripts/paths.py
+++ src/genomake/pipelines/chromake/scripts/paths.py
@@ -16,32 +16,36 @@ def get_all_fastq_related_paths(cfg: dict, mode: str):
             for sample in sequencing_data.get("SAMPLES", {}).values():
                 res.append(base / sample["R1"])
                 res.append(base / sample["R2"])
-            for inp in sequencing_data.get("INPUT", {}).values():
-                res.append(base / inp["R1"])
-                res.append(base / inp["R2"])
+            if "INPUT" in sequencing_data:
+                for inp in sequencing_data.get("INPUT", {}).values():
+                    res.append(base / inp["R1"])
+                    res.append(base / inp["R2"])
         elif mode == "fastqc_raw":
             for sample in sequencing_data.get("SAMPLES", {}).values():
                 res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R1"]).name))
                 res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R2"]).name))
-            for inp in sequencing_data.get("INPUT", {}).values():
-                res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
-                res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
+            if "INPUT" in sequencing_data:
+                for inp in sequencing_data.get("INPUT", {}).values():
+                    res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
+                    res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
         elif mode == "multiqc_raw":
             res.append(base / "QC/MULTIQC/RAW/multiqc_report.html")
         elif mode == "cutadapt":
             for sample in sequencing_data.get("SAMPLES", {}).values():
                 res.append(base / "TRIMMED" / Path(sample["R1"]).name)
                 res.append(base / "TRIMMED" / Path(sample["R2"]).name)
-            for inp in sequencing_data.get("INPUT", {}).values():
-                res.append(base / "TRIMMED" / Path(inp["R1"]).name)
-                res.append(base / "TRIMMED" / Path(inp["R2"]).name)
+            if "INPUT" in sequencing_data:
+                for inp in sequencing_data.get("INPUT", {}).values():
+                    res.append(base / "TRIMMED" / Path(inp["R1"]).name)
+                    res.append(base / "TRIMMED" / Path(inp["R2"]).name)
         elif mode == "fastqc_trimmed":
             for sample in sequencing_data.get("SAMPLES", {}).values():
                 res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R1"]).name))
                 res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R2"]).name))
-            for inp in sequencing_data.get("INPUT", {}).values():
-                res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
-                res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
+            if "INPUT" in sequencing_data:
+                for inp in sequencing_data.get("INPUT", {}).values():
+                    res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
+                    res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
         elif mode == "multiqc_trimmed":
             res.append(base / "QC/MULTIQC/TRIMMED/multiqc_report.html")
         elif mode == "bam":
@@ -65,16 +69,18 @@ def get_sequencing_fastq_related_paths(cfg: dict,
             for sample in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
                 res.append(base / sample["R1"])
                 res.append(base / sample["R2"])
-            for inp in cfg["SEQUENCING"][project_name].get("INPUT", {}).values():
-                res.append(base / inp["R1"])
-                res.append(base / inp["R2"])
+            if "INPUT" in cfg["SEQUENCING"][project_name]:
+                for inp in cfg["SEQUENCING"][project_name].get("INPUT", {}).values():
+                    res.append(base / inp["R1"])
+                    res.append(base / inp["R2"])
         elif mode == "fastqc_raw":
             for sample in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
                 res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R1"]).name))
                 res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R2"]).name))
-            for inp in cfg["SEQUENCING"][project_name].get("INPUT", {}).values():
-                res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
-                res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
+            if "INPUT" in cfg["SEQUENCING"][project_name]:
+                for inp in cfg["SEQUENCING"][project_name].get("INPUT", {}).values():
+                    res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
+                    res.append(base / "QC/FASTQC/RAW" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
         elif mode == "multiqc_raw":
             res.append(base / "QC/MULTIQC/RAW/multiqc_report.html")
         
@@ -82,23 +88,26 @@ def get_sequencing_fastq_related_paths(cfg: dict,
             for sample in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
                 res.append(base / "TRIMMED" / Path(sample["R1"]).name)
                 res.append(base / "TRIMMED" / Path(sample["R2"]).name)
-            for inp in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
-                res.append(base / "TRIMMED" / Path(inp["R1"]).name)
-                res.append(base / "TRIMMED" / Path(inp["R2"]).name)
+            if "INPUT" in cfg["SEQUENCING"][project_name]:
+                for inp in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
+                    res.append(base / "TRIMMED" / Path(inp["R1"]).name)
+                    res.append(base / "TRIMMED" / Path(inp["R2"]).name)
         elif mode == "fastqc_trimmed":
             for sample in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
                 res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R1"]).name))
                 res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(sample["R2"]).name))
-            for inp in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
-                res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
-                res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
+            if "INPUT" in cfg["SEQUENCING"][project_name]:
+                for inp in cfg["SEQUENCING"][project_name].get("SAMPLES", {}).values():
+                    res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R1"]).name))
+                    res.append(base / "QC/FASTQC/TRIMMED" / re.sub(r"\.fastq(\.gz)?$", "_fastqc.html", Path(inp["R2"]).name))
         elif mode == "multiqc_trimmed":
             res.append(base / "QC/MULTIQC/TRIMMED/multiqc_report.html")
         elif mode == "bam":
             for sample_name, sample_data in cfg["SEQUENCING"][project_name]["SAMPLES"].items():
                 res.append(base / "BAM" / (sample_name + ".bam"))
-            for input_name, sample_data in cfg["SEQUENCING"][project_name]["INPUT"].items():
-                res.append(base / "BAM" / (input_name + ".bam"))
+            if "INPUT" in cfg["SEQUENCING"][project_name]:
+                for input_name, sample_data in cfg["SEQUENCING"][project_name]["INPUT"].items():
+                    res.append(base / "BAM" / (input_name + ".bam"))
     else:
         print(f"There is an error, the configuration file don't contains a {project_name} project!")
     return res
